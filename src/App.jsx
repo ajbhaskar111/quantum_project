4118bc31d48398982dabe5ad595dbef6f07319e9
ajbhaskar111 <ajbhaskar111@gmail.com>
2024-06-14 18:42:25 +0530

    lates code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './container/Header';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import 'primeflex/primeflex.scss';
+
+
+
+const App = () => {
+  return (
+    <>
+
+    <Header />
+   </>
+  );
+}
+
+export default App;
