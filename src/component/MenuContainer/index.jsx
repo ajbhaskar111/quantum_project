4c209873cfbcfce3c6c978729fab1407cec2166a
mahesh jalodra <mahesh.jalodra@zgcns.com>
2024-07-05 22:58:58 +0530

    Mobile view menu added

--- src/component/MenuContainer/index.jsx
+++ src/component/MenuContainer/index.jsx
@@ -4,6 +4,36 @@ import { ListBox } from "primereact/listbox";
 import { useNavigate } from "react-router-dom";
 import { BrowserView, isMobile, MobileView } from "react-device-detect";
 import { Accordion, AccordionTab } from "primereact/accordion";
+import styled from "styled-components";
+
+let CustomAccordianDiv = styled.div`
+  .p-accordion-header-link {
+    border-radius: 0;
+    background-color: #fff;
+    border: 0;
+    i {
+      margin-top: -8px;
+      position: absolute;
+      right: 0.5em;
+      top: 50%;
+    }
+  }
+
+  .p-accordion-tab-active {
+    .p-accordion-header-link,
+    .p-accordion-content {
+      background: #f5f4f1;
+    }
+
+    .p-accordion-content {
+      padding: 0;
+    }
+  }
+
+  .p-accordion .p-accordion-tab {
+    margin-bottom: 0px;
+  }
+`;
 
 export const MenuContainer = (props) => {
   const menu = useRef(null);
@@ -33,15 +63,38 @@ export const MenuContainer = (props) => {
     );
   };
 
+  const AccordionHeader = (menuObj) => {
+    return (
+      <span className="flex align-items-center gap-2 w-full">
+        <span className="white-space-nowrap">{menuObj.label}</span>
+      </span>
+    );
+  };
+
   return (
     <div className="absolute w-full bg-white top-69">
       <MobileView>
-        <div className="card">
-          <Accordion multiple={true}>
+        <CustomAccordianDiv className="card">
+          <Accordion
+            multiple={true}
+            collapseIcon={
+              <i
+                className="pi pi-angle-down"
+                style={{ fontSize: "1.2rem", marginRight: "3px" }}
+              ></i>
+            }
+            expandIcon={
+              <i
+                className="pi pi-angle-up"
+                style={{ fontSize: "1.2rem", marginRight: "3px" }}
+              ></i>
+            }
+            transitionOptions={{ timeout: 0 }}
+          >
             {menuItem &&
               menuItem.map((menuObj) => {
                 return (
-                  <AccordionTab header={menuObj.label}>
+                  <AccordionTab header={AccordionHeader(menuObj)}>
                     <ListBox
                       value={selectedChildMenu}
                       onChange={(e) => setSelectedChildMenu(e.value)}
@@ -54,7 +107,7 @@ export const MenuContainer = (props) => {
                 );
               })}
           </Accordion>
-        </div>
+        </CustomAccordianDiv>
       </MobileView>
 
       <BrowserView>
@@ -81,6 +134,7 @@ export const MenuContainer = (props) => {
                   optionLabel="label"
                   className="w-full bg-transparent border-none shadow-none border-noround p-0"
                   itemTemplate={menuItemTemplate}
+                  style={{ backgroundColor: "#F5F4F1" }}
                 />
               )}
             </div>
