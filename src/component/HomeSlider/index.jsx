99e8c07796833ad743a6e8c1661ce33402bbc455
mahesh jalodra <mahesh.jalodra@zgcns.com>
2024-07-06 20:11:59 +0530

    Home slider sample added

--- src/component/HomeSlider/index.jsx
+++ src/component/HomeSlider/index.jsx
@@ -1,5 +1,30 @@
 import React, { useEffect, useRef, useState } from "react";
+import { Image } from "primereact/image";
 
 export const HomeSlider = (props) => {
-  return <p>HomeSlider</p>;
+  return (
+    <>
+      <div className="card flex justify-content-center">
+        <Image src="media/home_banners.png" alt="Image" width="100%" />
+        <div
+          style={{
+            position: "absolute",
+            bottom: "15%",
+            color: "#F5F4F1",
+          }}
+        >
+          <p style={{ fontSize: 30 }}>
+            Mastering <i>real estate</i> complexities,
+            <br /> shaping investments.
+          </p>
+
+          <p style={{ fontSize: 15 }}>
+            We offer a comprehensive range of end-to-end Real Estate Development
+            <br />
+            Business services to shape your ideas into revality
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
