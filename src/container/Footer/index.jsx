709166a7d680ac764a0acf0dc9f277547014aafc
ajbhaskar111 <131883954+ajbhaskar111@users.noreply.github.com>
2024-07-07 20:34:36 +0530

    Add the container box

--- src/container/Footer/index.jsx
+++ src/container/Footer/index.jsx
@@ -1,5 +1,22 @@
 import React, { useRef, useState } from "react";
 
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="footer_secition thmbg_gray text-white section_padding">
+      <div className="container">
+        <div className="grid">
+          <div className="lg:col-4 md:col-4 sm:col-12">
+             <h3 className="title">column 1</h3>
+          </div>
+          <div className="lg:col-4 md:col-4 sm:col-12">
+             <h2>column 2</h2>
+          </div>
+          <div className="lg:col-4 md:col-4 sm:col-12">
+             <h2>column 3</h2>
+          </div>
+        </div>
+        
+      </div>
+    </footer>
+  );
 };
